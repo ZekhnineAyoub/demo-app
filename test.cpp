7da21942da428a412cd1631421e5d9ca80cdb967
Ayoub Zekhnine <Ayoub.Zekhnine@efs.sante.fr>
2020-12-23 19:40:59 +0100

    mon dernier push

--- test.cpp
+++ test.cpp
@@ -1,6 +1,7 @@
 // wxWidgets "Hello world" Program
 // For compilers that support precompilation, includes "wx/wx.h".
 #include <wx/wxprec.h>
+#include <stdio.>
 #ifndef WX_PRECOMP
     #include <wx/wx.h>
 #endif
